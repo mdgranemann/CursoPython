7ae1b7711fd6a1fde6d02f6cbf92b25669c2d06c
Maykon Dyego Granemann <maykon.granemann@PROWAY.TREINA>
2020-06-13 11:47:37 -0300

    Aula2-For com lista

---
+++ Aula2/03_repeticao2.py
@@ -0,0 +1,10 @@
+lista_idades = [10,15,21,48]
+
+for i in range(5):
+    print(i)
+# Percorrendo a lista com for e range
+for i in range(len(lista_idades)):
+    print(lista_idades[i])
+# Percorrendo a lista com 'foreach'
+for i in lista_idades:
+    print(i)
